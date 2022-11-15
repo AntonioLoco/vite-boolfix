e65f37a18f3b39f6f4549aa5cf8efad339628b37
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-11-15 14:27:51 +0100

    Change size of flag

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -40,7 +40,7 @@ export default{
                     <h4 v-if="getName !== getOriginalName">Titolo Originale: </h4><span v-if="getName !== getOriginalName">{{getOriginalName}}</span>
                 </div>
                 <div class="language mb-2">
-                    <country-flag :country="`${getLanguage}`" size="small" />
+                    <country-flag :country="`${getLanguage}`" size="medium" />
                 </div>
                 <div class="stars mb-2">
                     <h2>Voto: </h2>
